9eaf0b39214784f157563df4d4d6b6594a58e70c
shikhabaljeetkhatri6-cmd <shikhabaljeetkhatri6@gmail.com>
2026-09-17 14:19:15 +0530

    updated studentData

--- lecture 12/data/studentData.js
+++ lecture 12/data/studentData.js
@@ -1,6 +1,6 @@
 const students=[
-    {rollNo:1, name:"Abhijeet", section:"BCA-D"},
-    {rollNo:2, name:"Upen", section:"BCA-D"},
+    {rollNo:1, name:"shikha", section:"BCA-D"},
+    {rollNo:2, name:"amisha", section:"BCA-D"},
     {rollNo:3, name:"Rahul", section:"BCA-D"},
     {rollNo:4, name:"Gurpreet", section:"BCA-D"},
     {rollNo:5, name:"Daulat", section:"BCA-D"}
